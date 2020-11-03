54319fabe6b614a0b2ecb41c720663fc0d8257af
mustafa <mustafagamaldeveloper@gmail.com>
2020-11-04 00:29:26 +0200

    Create home template, Create styling for signin and home templates

--- src/src/settings.py
+++ src/src/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # third part
     'rest_framework',
+    'crispy_forms',
     # my apps
     'twitter.apps.TwitterConfig',
 ]
@@ -122,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Bootstrap
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
