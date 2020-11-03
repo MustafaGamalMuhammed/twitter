82ffda9825b23beebf5593f4d1611ec563b79014
mustafa <mustafagamaldeveloper@gmail.com>
2020-11-03 07:30:03 +0200

    Create twitter app

--- src/src/settings.py
+++ src/src/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third part
+    'rest_framework',
+    # my apps
+    'twitter.apps.TwitterConfig',
 ]
 
 MIDDLEWARE = [
